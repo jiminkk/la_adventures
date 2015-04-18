f39e778cb0c7702b6a631f938ee84ed3b0fabfd3
Jasmine Kim <jasminekim95@yahoo.com>
2015-04-18 16:35:03 -0700

    added aboutme

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/info'
+
   resources :adventures
   root 'adventures#index'
   # The priority is based upon order of creation: first created -> highest priority.
